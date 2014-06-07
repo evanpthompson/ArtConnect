c7da0d1a2901f27a9c31c6678e746e139bbf39cd
matao <mattowens11@gmail.com>
2014-06-07 17:37:51 -0500

    Added museums controller and index view

---
+++ app/controllers/museums_controller.rb
@@ -0,0 +1,29 @@
+class MuseumsController < ApplicationController
+  def index
+    @museums = Museum.all
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+end
---
+++ test/controllers/museums_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class MuseumsControllerTest < ActionController::TestCase
+  test "index" do
+    get :index
+  end
+end
